baa1a7d4498e433bbd9e42c20fefb00db88d7559
Cihat Akviranli <cihatakv@gmail.com>
2020-03-11 20:25:34 -0500

    Driver factory class created for WebDriver object creation

--- src/test/java/utilities/DriverFactory.java
+++ src/test/java/utilities/DriverFactory.java
@@ -1,4 +1,19 @@
 package utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class DriverFactory {
+    public static WebDriver createADriver(String browserName) {
+        if (browserName.equalsIgnoreCase("Chrome")) {
+            // to Fix [1583367588.761][SEVERE]: Timed out receiving message from renderer: 0.100
+            WebDriverManager.chromedriver().version("79.0").setup();
+            return new ChromeDriver();
+        } else {
+            browserName.equalsIgnoreCase("firefox");
+            return new FirefoxDriver();
+        }
+    }
 }
